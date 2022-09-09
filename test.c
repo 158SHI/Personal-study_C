811379626ea07bd7b6f4a4758224731759d435a4
158SHI <111267010+158SHI@users.noreply.github.com>
2022-09-09 21:57:31 +0800

    9.9_review
    9.9_review

--- test.c
+++ test.c
@@ -1,44 +1,79 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
-//搬砖问题
-//一个男人搬3块砖，一个女人搬2块砖，两个小孩搬1块砖，需要n个人搬n块砖,一次搬完
-//共有多少种搬法
-//#include<stdio.h>
-//int main(void)
-//{
-//	int x, y, z = 0;
-//	int count = 0;
-//	int n = 0;
-//	printf("请输入 n=");
-//	scanf("%d", &n);
-//	for ( x = 0; x <= n/3; x++)
-//	{
-//		for ( y = 0; y <= n/2; y++)
-//		{
-//			for ( z = 0; z <= 2*n;z= z+2)
-//			{
-//				if (x+y+z==n && 3*x+2*y+z/2==n)
-//				{
-//					printf("men=%d women=%d kids=%d\n", x, y, z);
-//					count++;
-//				}
-//			}
-//		}
-//	}
-//	printf("count=%d\n", count);
-//	return 0;
-//}
+//三子棋
 
-//移位操作符
-#include<stdio.h>
+#include"game.h"
+
+void menu()
+{
+	printf("***----------------------***\n");
+	printf("***---1.play -- 0.exit---***\n");
+	printf("***----------------------***\n");
+}
+
+void game()
+{
+	char ret = 0;
+	//创建棋盘存放棋子信息
+	char board[ROW][COL] = { 0 };
+	InitBoard(board,ROW,COL);//初始化棋盘
+	DisplayBoard(board,ROW,COL);//打印棋盘
+	//下棋
+	while (1)
+	{
+		PlayerMove(board, ROW, COL);//玩家下
+		system("cls");
+		DisplayBoard(board, ROW, COL);//打印棋盘
+		ret=Iswin(board,ROW,COL);//判断输赢
+		if (ret!='C')
+		{
+			break;
+		}
+		ComputerMove(board, ROW, COL);//电脑下
+		system("cls");
+		DisplayBoard(board, ROW, COL);//打印棋盘
+		ret=Iswin(board,ROW,COL);
+		if (ret!='C')
+		{
+			break;
+		}
+	}
+	if (ret='*')
+	{
+		printf("恭喜你，获得胜利！\n");
+	}
+	else if(ret='#')
+	{
+		printf("很遗憾，你失败了！\n");
+	}
+	else
+	{
+		printf("双方平局\n");
+	}
+}
+
 int main(void)
 {
-	int a = 16;
-	int a1 = a >> 1;//右移一位等价于除以2
-	int b = -1;
-	int b1 = b >> 1;//整形在内存中存储的是补码
-	int c = 4;
-	int c1 = c << 1;//左移一位等价于乘以2
-	printf("a1=%d b1=%d c1=%d\n", a1, b1,c1);
+	srand((unsigned int)time(NULL));
+	int input = 0;
+	do
+	{
+		menu();
+		printf("请选择>:");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 2:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("选择错误，请重新输入\n");
+			break;
+		}
+
+	} while (input);
 	return 0;
 }
