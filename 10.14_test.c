09a53a21ca5f4bb501337ff43cde32491bd9ef60
158SHI <111267010+158SHI@users.noreply.github.com>
2022-10-14 19:13:00 +0800

    10.14_test

---
+++ 10.14_test.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include<stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	printf("你想怎么做？\n");
+//	printf("1.学习  0.摆烂\n>:");
+//	scanf("%d", &n);
+//	if (n==1)
+//	{
+//		printf("变优秀\n");
+//	}
+//	else
+//	{
+//		printf("卖红薯\n");
+//	}
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<math.h>
+//int main()
+//{
+//	int n, i;
+//	scanf("%d", &n);
+//	for (i = 2; i < sqrt(i); i++)
+//	{
+//		if (n%i == 0)
+//		{
+//			break;
+//		}
+//	}
+//	if (i>=sqrt(i))
+//	{
+//		;//是素数
+//	}
+//	else
+//	{
+//		;//不是素数
+//	}
+//	return 0;
+//}
+
+#include<stdio.h>
+int main()
+{
+	float a = 4.14f;//默认为double型，加f为float型
+	double b = 3.14;
+	int c = 12;
+	long c = 34;//默认是int类型
+	long d = 345L;//加后缀L为long型
+	long x = 1L;
+	return 0;
+}
