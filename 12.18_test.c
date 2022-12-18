38819a7f9ff6e9a7239a504b625acbc0664c29f8
158SHI <111267010+158SHI@users.noreply.github.com>
2022-12-18 23:52:37 +0800

    12.18

--- 12.18_test.c
+++ 12.18_test.c
@@ -2,3 +2,32 @@
 
 #include<stdbool.h>
 #include<stdio.h>
+
+int Func(int* nums, int numsSize)
+{
+    int left = 0;
+    int right = numsSize - 1;
+    int mid = 0;
+    while (left < right)
+    {
+        mid = left + (right - left) / 2;
+        if (nums[mid] < 0)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid;
+        }
+    }
+    return left;
+}
+
+int main()
+{
+    int arr[] = { 1,2,3,3,10 };
+    //int arr[] = { -5,-4,-3,-2 };
+    int sz = sizeof(arr) / sizeof(arr[0]);
+    printf("%d\n", Func(arr, sz));
+    return 0;
+}
