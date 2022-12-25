c34fb7f0592f0582c5d796dfa7eaa5d63cad24da
158SHI <111267010+158SHI@users.noreply.github.com>
2022-12-25 20:26:13 +0800

    Add files via upload

---
+++ 12.25_test.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int main()
+{
+	//printf("%c", '\xa');
+	//int arr[5 + 10] = { 0,1,2,3,4,6,5,5,3 };
+	//char c[] = { '1','2', '3', '4' };
+	int b;
+	char c[10];
+	scanf("%d%s", &b, &c);
+	printf("%d %s", b, c);
+	return 0;
+}
