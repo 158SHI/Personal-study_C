4cba847197ac6a47f24cf45f196a2a3ca98b954d
158SHI <111267010+158SHI@users.noreply.github.com>
2022-12-29 21:10:25 +0800

    12.29

---
+++ 12.29_test.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdbool.h>
+#include<stdio.h>
+
+//int main()
+//{
+//	bool b = true;
+//	printf("%d\n", b);
+//	printf("%zd\n", sizeof(true));
+//	return 0;
+//}
+
+int main()
+{
+	if (-1)
+	{
+		printf("hehe\n");
+	}
+	if (0)
+	{
+		printf("haha\n");
+	}
+	return 0;
+}
